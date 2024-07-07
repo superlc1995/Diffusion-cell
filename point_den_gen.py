f4dc71a463c9ff93651cf7359d1cf5027dd8a2b4
Li <lchen@harrier.cm.cluster>
2024-07-07 15:45:13 -0400

    dataset preprocessing

--- point_den_gen.py
+++ point_den_gen.py
@@ -3,17 +3,17 @@ from skimage import morphology
 import numpy as np
 from PIL import Image
 
-file_list = np.loadtxt('/home/lchen/tensorf/Dataset-BRCA-M2C/brca_ds_train.txt', dtype = 'str')
+file_list = np.loadtxt('Dataset-BRCA-M2C/brca_ds_train.txt', dtype = 'str')
 
 for file in file_list:
     # print(file)
-    img = np.array(Image.open('/home/lchen/tensorf/Dataset-BRCA-M2C/images/' + file))
+    img = np.array(Image.open('Dataset-BRCA-M2C/images/' + file))
     out_den = np.zeros([int(img.shape[0]/2) + 1, int(img.shape[1]/2) + 1, 3])
 
     for type in [1,2,3]:
         
         # kde_den = np.zeros( [ int(img.shape[0]/2), int(img.shape[1]/2), 3] )
-        labels = np.loadtxt( '/home/lchen/tensorf/Dataset-BRCA-M2C/labels/' + file.split('.png')[0] + '_gt_class_coords.txt' )
+        labels = np.loadtxt( 'Dataset-BRCA-M2C/labels/' + file.split('.png')[0] + '_gt_class_coords.txt' )
         labels = labels[labels[:,2] == type]
         labels = labels/2
 
